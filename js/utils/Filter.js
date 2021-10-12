727c959b64106533da7ad2c33004bc01ff35f507
agustin gomez <navarro571g@gmail.com>
2021-10-12 06:54:25 -0300

    Filter improved

--- js/utils/Filter.js
+++ js/utils/Filter.js
@@ -1,26 +1,31 @@
 class PokemonFilter {
-  #source;
   #filtered;
   constructor(source) {
-    this.#source = source;
     this.#filtered = source;
   }
 
+  /**
+   *
+   * @param {*} param Card key parameter
+   * @param {*} op Operation
+   * @param {*} value Value to search
+   * @returns PokemonFilter
+   */
   search(param, op, value) {
     this.#filtered = this.#filtered.filter((card) => {
       let key = param(card);
       switch (op) {
         case "=":
-            return key.toUpperCase().includes(value.toUpperCase());
+          return key.toUpperCase().includes(value.toUpperCase());
         case ">":
-            return key > value;
+          return key > value;
         case "<":
-            return key < value;
+          return key < value;
       }
     });
     return this;
   }
-  
+
   get() {
     return this.#filtered;
   }
